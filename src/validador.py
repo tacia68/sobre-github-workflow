580bbbaa765b33ffe813a02827c510f526401c47
BRUNNO-R <brunoramosro@gmail.com>
2023-04-28 14:26:18 -0300

    alter

--- src/validador.py
+++ src/validador.py
@@ -16,11 +16,42 @@ def validar_cep(cep):
     else:
         return False
   
+
 def validar_cpf(cpf):
-  # escreva seu código aqui
-  
+    cpf = re.sub('[^0-9]', '', cpf) # remove todos os caracteres não numéricos
+    if len(cpf) != 11: # verifica se o CPF tem 11 dígitos
+        return False
+
+    # realiza o cálculo dos dígitos verificadores
+    soma = 0
+    for i in range(9):
+        soma += int(cpf[i]) * (10 - i)
+    resto = 11 - (soma % 11)
+    if resto == 10 or resto == 11:
+        resto = 0
+    if resto != int(cpf[9]):
+        return False
+
+    soma = 0
+    for i in range(10):
+        soma += int(cpf[i]) * (11 - i)
+    resto = 11 - (soma % 11)
+    if resto == 10 or resto == 11:
+        resto = 0
+    if resto != int(cpf[10]):
+        return False
+
+    return True # CPF válido
 def validar_telefone(telefone):
-  # escreve seu código aqui
+    telefone = re.sub('[^0-9]', '', telefone) # remove todos os caracteres não numéricos
+    if len(telefone) != 11 and len(telefone) != 10: # verifica se o telefone tem 10 ou 11 dígitos
+        return False
+
+    # verifica se o primeiro dígito é 9 (caso de celular) ou 2 a 5 (caso de telefone fixo)
+    if not re.match('^[2-5|9]{1}', telefone):
+        return False
+
+    return True # telefone válido
   
  
   
